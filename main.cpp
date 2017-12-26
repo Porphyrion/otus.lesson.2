be7ae9dd4df6151984c89c690b8245ba6bd474ce
Ivan <ivanbasurman88@yandex.ru>
2017-12-26 15:20:08 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -37,12 +37,20 @@ auto ipTuple(std::vector<std::string> && x)->decltype(std::make_tuple(1,2,3,4)) 
 //filter by first or first and second bytes and output
 template <typename... Args>
 void filter(std::vector<ip>::const_iterator b, std::vector<ip>::const_iterator e, Args ...args) {
-    auto byteTuple = std::make_tuple((args)...); 
+    auto byteTuple = std::make_tuple((args)...);
+    if(sizeof...(args) == 1){
     std::for_each(b, e, [byteTuple](ip x){
-        if(!(byteTuple != x))  {
+        if(std::get<0>(byteTuple) = std::get<0>(x))  {
+            writeIp(x);
+        }
+    });}
+    else if(sizeof...(args) == 2){
+            std::for_each(b, e, [byteTuple](ip x){
+        if(std::get<0>(byteTuple) = std::get<0>(x) && std::get<1>(byteTuple) = std::get<1>(x))  {
             writeIp(x);
         }
     });
+    }
 }
 
 //filter by any byte and output
